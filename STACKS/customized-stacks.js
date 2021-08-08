e63d107296254290b8ed47e42bdebdc7b9cf940a
Jay <jayjani008@gmail.com>
2021-08-08 15:20:45 +0530

    Corrected bug - myStack value not updating previously

--- STACKS/customized-stacks.js
+++ STACKS/customized-stacks.js
@@ -1,37 +1,43 @@
-const Stack = function() {
-    this.count = 0;
-    this.items = {};
+const Stack = function () {
+  this.count = 0;
+  this.items = {};
 
-    this.push = function(value) {
-        this.count++;
-        this.items[this.count] = value;
-        return this.items[this.count];
+  this.push = function (value) {
+    this.items[this.count] = value;
+    this.count++;
+  };
+
+  this.pop = function () {
+    if (this.count === 0) {
+      return "Empty Stack";
     }
 
-    this.pop = function() {
-        if(this.count === 0) {
-            return 'Empty Stack';
-        }
+    this.count--;
+    let result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  };
 
-        this.count--;
-        // this.count+1 to show popped element 
-        return this.items[this.count+1];
-    }
+  this.size = function () {
+    return this.count;
+  };
 
-    this.size = function() {
-        return this.count;
-    }
+  this.peek = function () {
+    return this.items[this.count - 1];
+  };
+};
 
-    this.peek = function() {
-        return this.items[this.count];
-    }
-}
-
-const myStack = new Stack();
-console.log('push ' +myStack.push(1));
-console.log('push ' +myStack.push(2));
-console.log('push ' +myStack.push(3));
-console.log('peek ' +myStack.peek());
-console.log('size '+ myStack.size());
-console.log('pop ' +myStack.pop());
-console.log('peek ' +myStack.peek());
+let myStack = new Stack();
+console.log("push " + myStack.push(1));
+console.log("push " + myStack.push(2));
+console.log("push " + myStack.push(3));
+console.log("peek " + myStack.peek());
+console.log("size " + myStack.size());
+console.log("pop " + myStack.pop());
+console.log({myStack});
+console.log("peek " + myStack.peek());
+console.log("pop " + myStack.pop());
+console.log({myStack});
+console.log("peek " + myStack.peek());
+console.log("pop " + myStack.pop());
+console.log("peek " + myStack.peek());
