162ec5df6218cc51c045cd53f84acc88bddd52e5
Jay <jayjani008@gmail.com>
2021-08-12 23:53:32 +0530

    Added Priority Queue info.

---
+++ PRIORITYQUEUE/priorityQueue.js
@@ -0,0 +1,4 @@
+// PRIORITY QUEUE
+// Not only pass the value but also its priority(number).
+// Higher the priority, ahead in the queue.
+// If values have same priority, it behaves like a normal queue.
