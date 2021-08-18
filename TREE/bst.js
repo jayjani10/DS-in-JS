69f91f35949da17ae34e702fd363946637bf310f
Jay <jayjani008@gmail.com>
2021-08-18 23:38:30 +0530

    Tree data structure info.

---
+++ TREE/bst.js
@@ -0,0 +1,6 @@
+// TREES
+// Consists of a tree structure
+// All nodes are objects
+// Each node has a name, a parent, and a list of children
+// The root node has no parent
+// The root node has a list of children
